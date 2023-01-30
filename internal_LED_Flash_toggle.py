2d86291440935de06995cbd266ce121409c2eae3
Lori Pfahler <lpfahler@comcast.net>
2023-01-30 16:55:41 -0500

    First upload

---
+++ internal_LED_Flash_toggle.py
@@ -0,0 +1,11 @@
+import machine
+# utime and time are the same - the time library just points to utime library
+from utime import sleep
+
+# on regular pico can use GP25 - on pico w use 'LED"
+#  for regular pico use: internalLED = machine.Pin(25, machine.Pin.OUT
+internalLED = machine.Pin('LED', machine.Pin.OUT)
+
+while True:
+    internalLED.toggle()
+    sleep(1)
